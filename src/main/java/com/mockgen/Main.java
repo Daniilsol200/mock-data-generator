ed713a287391abcf16c8b18d5587fa24c4ea1b10
Ковалевский Даниил <dogeboy556@gmail.com>
2025-11-13 11:55:16 +0300

    Разработка тестов

--- src/main/java/com/mockgen/Main.java
+++ src/main/java/com/mockgen/Main.java
@@ -21,7 +21,7 @@ public class Main {
                 generator.generate(writer, formatter);
             }
         } catch (Exception e) {
-            System.err.println("Error: " + e.getMessage());
+            System.err.println("Error1: " + e.getMessage());
             e.printStackTrace();
         }
     }
